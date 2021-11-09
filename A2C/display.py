9ae37ff777343af6f597d387874d813ccde75e9a
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-11-09 11:46:26 +0100

    All works but the behaviour of the agent is strange

--- A2C/display.py
+++ A2C/display.py
@@ -1,13 +1,12 @@
 from main import World
 from model import DisplayAgent
-from model import A2CNet
 import torch
 from matplotlib import pyplot as plt
 import time
 from gym import spaces
 
 
-def plot_step(dis, val, act, title):
+def plot_step(dis, val, act):
     names = act
     values = dis
     plt.subplot(211)
@@ -22,16 +21,16 @@ def main():
     # number of episodes that I want to visualize
     num_episodes = 10
     name_acts = ["STOP", "UP", "DOWN", "LEFT", "RIGHT", "BOMB"]
-    title = "P(a|s) and V(s)"
     world = World()
     env = world.env
     model = world.model
     model.load_state_dict(torch.load("convrnn-s.weights", map_location='cpu'))
     display_agent = DisplayAgent(model)
-    display_agent.clear()
-    ded, state, _ = False, env.reset(), world.leif.clear()
 
     for i in range(num_episodes):
+
+        display_agent.clear()
+        ded, state, _ = False, env.reset(), world.leif.clear()
         print("Episode {}".format(i+1))
         done = False
         j = 0
@@ -49,7 +48,7 @@ def main():
 
             env.render()
             time.sleep(0.5)
-            plot_step(dist, value, name_acts, title)
+            plot_step(dist, value, name_acts)
             state, reward, done, info = env.step(actions)
             j += 1
 
