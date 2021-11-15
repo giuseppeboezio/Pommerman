91c403dc12b93171b92aabf03e21d059fa4b4fe5
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-11-15 22:46:10 +0100

    number of wooden walls destroyed for each position

--- heuristic_modules/module_1.py
+++ heuristic_modules/module_1.py
@@ -13,6 +13,7 @@ def get_feasible_pos(state):
 
 
 def destroyed_walls(state, position):
+    """Number of walls that can be destroyed putting a bomb in position"""
     count = 0
     positions = []
     blast_strength = state['blast_strength'] - 1
@@ -21,8 +22,56 @@ def destroyed_walls(state, position):
     right = [(position[0], position[1] + j) for j in range(1, blast_strength + 1)]
     up = [(position[0] - j, position[1]) for j in range(1, blast_strength + 1)]
     down = [(position[0] + j, position[1]) for j in range(1, blast_strength + 1)]
-
-
+    positions = positions + left + right + up + down
+    # Removing positions outside the board
+    for pos in positions:
+        if pos[0] < 0 or pos[1] < 0:
+            positions.remove(pos)
+    # positions where there are destructible walls
+    board = state['board']
+    positions = [pos for pos in positions if board[pos[0],pos[1]] == 2]
+    # left direction
+    left = [pos for pos in positions if pos[1] < position[1]]
+    for elem in left:
+        # checking whether the element on the position is a rigid wall
+        if board[elem[0], elem[1]] == 1:
+            remove_list = [pos for pos in left if pos[1] < elem[1]]
+            # removing positions beyond the rigid wall because they cannot be reached by the flames
+            for item in remove_list:
+                left.remove(item)
+    # right direction
+    right = [pos for pos in positions if pos[1] > position[1]]
+    for elem in right:
+        # checking whether the element on the position is a rigid wall
+        if board[elem[0], elem[1]] == 1:
+            remove_list = [pos for pos in right if pos[1] > elem[1]]
+            # removing positions beyond the rigid wall because they cannot be reached by the flames
+            for item in remove_list:
+                right.remove(item)
+    # up direction
+    up = [pos for pos in positions if pos[0] < position[0]]
+    for elem in up:
+        # checking whether the element on the position is a rigid wall
+        if board[elem[0], elem[1]] == 1:
+            remove_list = [pos for pos in up if pos[0] < elem[0]]
+            # removing positions beyond the rigid wall because they cannot be reached by the flames
+            for item in remove_list:
+                up.remove(item)
+    # down direction
+    down = [pos for pos in positions if pos[0] > position[0]]
+    for elem in down:
+        # checking whether the element on the position is a rigid wall
+        if board[elem[0], elem[1]] == 1:
+            remove_list = [pos for pos in down if pos[0] > elem[0]]
+            # removing positions beyond the rigid wall because they cannot be reached by the flames
+            for item in remove_list:
+                down.remove(item)
+    # removing positions that do not correspond to wooden walls
+    positions = left + right + up + down
+    # keep only positions where there are wooden walls
+    positions = [pos for pos in positions if board[pos[0],pos[1]] == 2]
+    count = len(positions)
+    return count
 
 
 def get_destroyed_boxes(state):
