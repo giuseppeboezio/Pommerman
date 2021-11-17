6d16fb622c334973e0cd6b049d30e2037f91f98c
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-11-17 22:41:58 +0100

    Final implementation of module 1, the two main functions must be tested

--- heuristic_modules/module_1.py
+++ heuristic_modules/module_1.py
@@ -1,6 +1,5 @@
 import numpy as np
 from playground.pommerman import constants
-from queue import PriorityQueue
 
 def get_feasible_pos(state):
     """Positions of the board where the agent can put a bomb"""
@@ -86,11 +85,33 @@ def get_destroyed_boxes(state):
     return bombs
 
 
-# dijkstra's algorithm for grids
-def dijkstra(board, start, end):
+class PriorityQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, elem):
+        """Enqueue an element in the form (priority, node)"""
+        self.queue.append(elem)
+        sorted(self.queue, key=lambda x: x[0])
+
+    def dequeue(self):
+        return self.queue.pop(0)
+
+    def change_priority(self, item, priority):
+        items = [elem[1] for elem in self.queue]
+        pos = items.index(item)
+        del self.queue[pos]
+        self.queue.append((priority,item))
+        sorted(self.queue, key=lambda x: x[0])
 
 
-    pass
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def has(self, item):
+        items = [elem[1] for elem in self.queue]
+        return item in items
 
 
 class Node:
@@ -98,19 +119,32 @@ class Node:
     def __init__(self, position):
         self.position = position
         self.neighbours = []
-        # distance from the root node, -1 means infinity
-        self.distance = -1
-        self.parent = None
+        # distance from the root node, 1000 means infinity
+        self.distance = np.inf
+        self.discovered = False
 
     def add_neighbour(self, neighbour):
         self.neighbours.append(neighbour)
 
-    def set_parent(self, parent):
-        self.parent = parent
+    def get_distance(self):
+        return self.distance
+
+    def set_distance(self, distance):
+        self.distance = distance
+
+    def set_discovered(self):
+        self.discovered = True
+
+    def is_discovered(self):
+        return self.discovered
+
+    def get_position(self):
+        return self.position
 
     def get_unvisited_neighbours(self):
 
-        pass
+        unvisited = [node for node in self.neighbours if not self.is_discovered()]
+        return unvisited
 
 
 # graph of the board
@@ -119,6 +153,14 @@ class Board:
     def __init__(self, board):
 
         self.nodes = []
+        self.root = None
+
+        # dictionary to store the nodes
+        nodes_dict = {}
+        for i in range(constants.BOARD_SIZE):
+            for j in range(constants.BOARD_SIZE):
+                if board[i,j] == 0 or board[i,j] == 10:
+                    nodes_dict[(i,j)] = Node((i,j))
 
         for i in range(constants.BOARD_SIZE):
             for j in range(len(constants.BOARD_SIZE)):
@@ -133,16 +175,20 @@ class Board:
                         if elem[0] < 0 or elem[1] < 0 or board[elem[0],elem[1]] != 0:
                             neighbours.remove(elem)
 
-                    node = Node((i,j))
-
-                    # adding neighbours of the node
                     for elem in neighbours:
-                        node.add_neighbour(Node((elem[0],elem[1])))
+                        nodes_dict[(i,j)].add_neighbour(nodes_dict[(elem[0],elem[1])])
 
-                    self.nodes.append(node)
+                    self.nodes.append(nodes_dict[(i,j)])
+
+                    # if in the position there is our agent it means that this node is the root
+                    if board[i,j] == 10:
+                        self.root = nodes_dict[(i,j)]
 
     def get_nodes(self):
         return self.nodes
+
+    def get_root_node(self):
+        return self.root
 
 
 def get_distances(state):
@@ -150,8 +196,40 @@ def get_distances(state):
     obs = state['board']
     board_graph = Board(obs)
 
+    # dijkstra's algorithm
+    # list of nodes to expand
+    priority_q = PriorityQueue()
 
-    pass
+    start = board_graph.get_root_node()
+    start.set_distance(0)
+
+    # adding root to the queue
+    priority_q.enqueue((0,start))
+
+    while not priority_q.is_empty() :
+        current_node = priority_q.dequeue()[1]
+        current_node.set_discovered()
+
+        for neighbour in current_node.get_unvisited_neighbours():
+            min_distance = min(neighbour.get_distance(), current_node.get_distance() + 1)
+
+            if min_distance != neighbour.get_distance():
+                neighbour.set_distance(min_distance)
+                if priority_q.has(neighbour):
+                    priority_q.change_priority(neighbour, min_distance)
+
+            if not priority_q.has(neighbour):
+                priority_q.enqueue((neighbour.get_distance(), neighbour))
+
+    distances = np.full((constants.BOARD_SIZE,constants.BOARD_SIZE), np.inf)
+
+    nodes = board_graph.get_nodes()
+    for node in nodes:
+        pos = node.get_position()
+        distances[pos[0],pos[1]] = node.get_distance()
+
+    return distances
+
 
 def combine_masks(bombs, distances, alpha=1, beta=0.5):
     return bombs * alpha - distances * beta
