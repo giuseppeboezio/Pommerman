70ddd0008b7b8ee27fe52bb4957a8e01b498ac22
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2022-01-03 17:25:01 +0100

    Game settings and print for testing

---
+++ agents/planner_agent/run_game.py
@@ -0,0 +1,26 @@
+from agent import PlannerAgent
+from pommerman import agents, make
+import time
+
+
+def main():
+
+    my_agent = PlannerAgent()
+    config = 'PommeFFACompetition-v0'
+    agent_list = [my_agent, agents.RandomAgent(), agents.SimpleAgent(), agents.RandomAgent()]
+    game_state_file = None
+
+    # Make the "Free-For-All" environment using the agent list
+    env = make(config, agent_list, game_state_file)
+
+    state = env.reset()
+    done = False
+    while not done:
+        env.render()
+        time.sleep(0.5)
+
+        actions = env.act(state)
+        state, reward, done, info = env.step(actions)
+
+
+main()
