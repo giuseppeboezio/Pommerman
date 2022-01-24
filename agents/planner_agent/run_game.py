bfe0f285272d8417d597014d7d811f0375eb5b8e
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2022-01-24 15:32:40 +0100

    stop agent implemented

--- agents/planner_agent/run_game.py
+++ agents/planner_agent/run_game.py
@@ -1,13 +1,25 @@
 from agent import PlannerAgent
 from pommerman import agents, make
 import time
+from pommerman.agents import BaseAgent
+from pommerman import constants
+
+
+# agent which returns always stop as move
+class StopAgent(BaseAgent):
+
+    def __init__(self):
+        super(StopAgent, self).__init__()
+
+    def act(self, obs, action_space):
+        return constants.Action.Stop
 
 
 def main():
 
     my_agent = PlannerAgent()
     config = 'PommeFFACompetition-v0'
-    agent_list = [my_agent, agents.SimpleAgent(), agents.SimpleAgent(), agents.SimpleAgent()]
+    agent_list = [my_agent, StopAgent(), StopAgent(), StopAgent()]
     game_state_file = None
 
     # Make the "Free-For-All" environment using the agent list
