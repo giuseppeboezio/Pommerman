4077c3438a48c784bae22c56da1a13df4342e091
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2022-01-31 23:44:58 +0100

    Basic implementation of kick bomb substrategy

--- agents/planner_agent/agent.py
+++ agents/planner_agent/agent.py
@@ -13,13 +13,23 @@ from utility import show_board
 
 
 def count_num_item(board, set_items):
-    # count number of cells containing certain kind of items in set_items
+    """count number of cells containing certain kind of items in set_items"""
     count = 0
     for i in range(constants.BOARD_SIZE):
         for j in range(constants.BOARD_SIZE):
             if board[i,j] in set_items:
                 count += 1
     return count
+
+
+def get_item_pos(board, items):
+    """get positions of item in items"""
+    positions = []
+    for i in range(constants.BOARD_SIZE):
+        for j in range(constants.BOARD_SIZE):
+            if board[i,j] in items:
+                positions.append((i,j))
+    return positions
 
 
 def generate_path(nodes, target_pos):
@@ -146,6 +156,22 @@ class Target(Enum):
     Collect = 3
 
 
+class KickStep(Enum):
+    """Phase of kicking bomb strategy"""
+    TargetDef = 0
+    Bomb = 1
+    Safe = 2
+    SingleStep = 3
+    Kick = 4
+
+
+class KickDirection(Enum):
+    Left = 0
+    Right = 1
+    Up = 2
+    Down = 3
+
+
 # agent behaves in the following way:
 # Target 1: put the bomb in the position where it is possible to destroy the highest number of wooden walls
 # Target 2: choose a position where to avoid being killed by a bomb
@@ -160,6 +186,8 @@ class PlannerAgent(BaseAgent):
         self.num_walls = 0  # Number of wooden walls
         self.num_pow_up = 0  # Number of found power-ups
         self.obs_board = None  # Observation to use for power-ups counting
+        self.killer_mode = KickStep.TargetDef.value  # flag to enable kicking bomb strategy
+        self.kick_direction = None  # Direction to kick the bomb
 
     def act(self, obs, action_space):
 
@@ -177,7 +205,7 @@ class PlannerAgent(BaseAgent):
         corrective_strategy = True
 
         # check whether the agent can kick the bombs to kill the opponents
-        if obs['can_kick'] and self.num_pow_up < self.num_walls / 2:
+        if not obs['can_kick'] or self.num_pow_up < self.num_walls / 2:
 
             # used to switch among more sub strategies before deciding next action
             while corrective_strategy:
@@ -360,7 +388,147 @@ class PlannerAgent(BaseAgent):
 
         # kicking bomb strategy
         else:
-            pass
+
+            corrective_strategy = True
+
+            while corrective_strategy:
+                # target where to place a bomb must be defined
+                if self.killer_mode == KickStep.TargetDef.value:
+                    # opponents' positions
+                    set_item = {Item.Agent1.value, Item.Agent2.value, Item.Agent3.value}
+                    agents_pos = get_item_pos(obs['board'], set_item)
+                    # changing board to detect the nearest agent
+                    new_obs = copy.copy(obs)
+                    new_board = change_board(obs['board'], agents_pos, Item.Passage.value)
+                    new_obs['board'] = new_board
+                    # execute Dijkstra's algorithm to get distances
+                    distances, nodes = get_distances(new_obs)
+                    # get target agent and positions where it is possible to place a bomb to kill it
+                    opp, dis = tg_four.get_target_opponent(agents_pos, distances)
+                    # it makes sense to look for a position only if it is possible to reach an opponent
+                    if opp is None:
+                        self.killer_mode = KickStep.Safe.value
+                    else:
+                        positions = tg_four.get_admissible_pos(opp, obs['board'])
+                        self.target_pos, self.kick_direction = tg_four.get_target_pos(distances, opp, positions)
+                        self.killer_mode = KickStep.Bomb.value
+
+                # the agent must reach the position where to place a bomb
+                if self.killer_mode == KickStep.Bomb.value:
+                    # find the path to place the bomb
+                    # use Dijkstra's algorithm to find distances
+                    distances, nodes = get_distances(obs)
+                    path = generate_path(nodes, self.target_pos)
+
+                    if len(path) > 0:
+                        # general concept: the bomb is dangerous only if it affects agent path
+                        # and if it explodes before the agent reaches the target position
+                        # obtaining positions of bomb and related explosion field
+                        dangerous_pos = tg_two.get_positions(obs)
+                        safe = is_path_safe(path, dangerous_pos)
+                        if safe:
+                            next_position = path[0]
+                            action = get_action(obs['position'], next_position)
+                            corrective_strategy = False
+                        else:
+                            # in case the path is not safe the agent moves toward a safe position
+                            self.defined = False
+                            self.target_pos = None
+                            self.killer_mode = KickStep.Safe.value
+                    else:
+                        # the current position of the agent is the target position
+                        if obs['position'][0] == self.target_pos[0] and obs['position'][1] == self.target_pos[1]:
+                            # check whether the agent has enough ammo
+                            if obs['ammo'] > 0:
+                                # the action must be passed as a value
+                                action = constants.Action.Bomb.value
+                                self.defined = False
+                                self.target_pos = None
+                                self.killer_mode = KickStep.SingleStep.value
+                                corrective_strategy = False
+                            else:
+                                # the agent must wait until it has again an ammo if the position is safe, move otherwise
+                                dangerous_pos = tg_two.get_positions(obs)
+                                safe_pos = is_pos_safe(self.target_pos, dangerous_pos)
+                                if safe_pos:
+                                    action = constants.Action.Stop
+                                    corrective_strategy = False
+                                else:
+                                    self.killer_mode = KickStep.Safe.value
+                                    self.defined = False
+                                    self.target_pos = None
+                        else:
+                            # case in which it is no more possible to reach the target
+                            self.defined = False
+                            self.killer_mode = KickStep.Safe.value
+
+                # the agent has placed the bomb and it has to move to kick it in the next timestep
+                if self.killer_mode == KickStep.SingleStep.value:
+                    if self.kick_direction == KickDirection.Left.value:
+                        action = constants.Action.Right
+                    elif self.kick_direction == KickDirection.Right.value:
+                        action = constants.Action.Left
+                    elif self.kick_direction == KickDirection.Down.value:
+                        action = constants.Action.Up
+                    else:
+                        action = constants.Action.Down
+                    corrective_strategy = False
+                    self.killer_mode = KickStep.Kick.value
+
+                # kick the bomb
+                if self.killer_mode == KickStep.Kick.value:
+                    if self.kick_direction == KickDirection.Left.value:
+                        action = constants.Action.Left
+                    elif self.kick_direction == KickDirection.Right.value:
+                        action = constants.Action.Right
+                    elif self.kick_direction == KickDirection.Down.value:
+                        action = constants.Action.Down
+                    else:
+                        action = constants.Action.Up
+                    corrective_strategy = False
+                    # TODO what to do after kicking the bomb
+
+                # safe position
+                if self.killer_mode == KickStep.Safe.value:
+                    # take into account positions where the agent could be killed in case the bomb explodes at this timestep
+                    expl_fields = tg_two.get_positions(obs)
+                    dangerous_pos = get_immediate_expl_pos(expl_fields)
+                    # exclude the position of the agent from the dangerous position
+                    if obs['position'] in dangerous_pos:
+                        dangerous_pos.remove(obs['position'])
+                    # create a board which takes into account possible dangerous positions
+                    new_board = change_board(obs['board'], dangerous_pos, Item.Rigid.value)
+                    new_obs = copy.copy(obs)
+                    new_obs['board'] = new_board
+
+                    if not self.defined:
+
+                        # use Dijkstra's algorithm to find distances and keep the closest position
+                        distances, nodes = get_distances(new_obs)
+                        self.target_pos = tg_two.get_target_pos(distances, new_obs['position'], new_obs['board'])
+                        # there is not a safe position for the agent (because in our strategy it must move)
+                        if self.target_pos[0] == obs['position'][0] and self.target_pos[1] == obs['position'][1]:
+                            return constants.Action.Stop
+                        else:
+                            self.defined = True
+
+                    # update the path
+                    distances, nodes = get_distances(new_obs)
+                    path = generate_path(nodes, self.target_pos)
+                    # at each step I check whether there is a path to reach the position or not
+                    if len(path) > 0:
+                        next_position = path[0]
+                        action = get_action(obs['position'], next_position)
+                        corrective_strategy = False
+                    else:
+                        # the current position of the agent is the target position
+                        if obs['position'][0] == self.target_pos[0] and obs['position'][1] == self.target_pos[1]:
+                            self.defined = False
+                            self.target_pos = None
+                            self.killer_mode = KickStep.TargetDef.value
+                        else:
+                            action = constants.Action.Stop
+                            corrective_strategy = False
 
         print(action)
 
--- agents/planner_agent/target_4.py
+++ agents/planner_agent/target_4.py
@@ -1,8 +1,11 @@
+from agent import KickDirection
 from pommerman.constants import Item
 from pommerman import constants
+import numpy as np
 
 
 def count_power_ups(board1, board2):
+    """ counting number of new power-up between two timesteps of the game"""
     items = {Item.ExtraBomb.value, Item.IncrRange.value, Item.Kick.value}
     pos_board1 = set()
     pos_board2 = set()
@@ -16,3 +19,81 @@ def count_power_ups(board1, board2):
     new_pos_pow_ups = pos_board2 - pos_board1
     count = len(new_pos_pow_ups)
     return count
+
+
+def get_target_opponent(opp_pos, distances):
+    """return the position of the closest opponent that can be reached by my agent"""
+    opponents_dis = {(i,j): distances[i,j] for (i,j) in opp_pos}
+    opp = None
+    dis_min = np.inf
+    for (pos, dis) in opponents_dis.items():
+        if dis < dis_min:
+            opp = pos
+            dis_min = dis
+    return opp, dis_min
+
+
+def get_admissible_pos(ag_pos, board):
+    """Return positions where it is possible to place a bomb and kick it toward the opponent"""
+    left = []
+    right = []
+    up = []
+    down = []
+
+    # left scan
+    i = ag_pos[0]
+    j = ag_pos[1] - 1
+    while j >= 0 and board[i,j] == Item.Passage.value:
+        left.append((i,j))
+        j -= 1
+
+    # right scan
+    i = ag_pos[0]
+    j = ag_pos[1] + 1
+    while j < constants.BOARD_SIZE and board[i,j] == Item.Passage.value:
+        right.append((i,j))
+        j += 1
+
+    # up scan
+    i = ag_pos[0] - 1
+    j = ag_pos[1]
+    while i >= 0 and board[i,j] == Item.Passage.value:
+        up.append((i,j))
+        i -= 1
+
+    # down scan
+    i = ag_pos[0] + 1
+    j = ag_pos[1]
+    while i < constants.BOARD_SIZE and board[i,j] == Item.Passage.value:
+        down.append((i,j))
+        i += 1
+
+    # remove last positions in all directions due to the needed space to kick the bomb
+    left.pop()
+    right.pop()
+    up.pop()
+    down.pop()
+
+    positions = left + right + up + down
+    return positions
+
+
+def get_target_pos(dis_board, agent_pos, positions):
+    """Return positions where to place a bomb and direction of kick"""
+    distances = []
+    for i in range(len(positions)):
+        distances.append(dis_board[positions[i][0], positions[i][1]])
+
+    target_pos = positions[np.argmin(distances)]
+    # check the kick direction
+    if target_pos[0] == agent_pos[0] and target_pos[1] < agent_pos[1]:
+        direction = KickDirection.Right.value
+    elif target_pos[0] == agent_pos[0] and target_pos[1] > agent_pos[1]:
+        direction = KickDirection.Left.value
+    elif target_pos[1] == agent_pos[1] and target_pos[0] < agent_pos[0]:
+        direction = KickDirection.Down.value
+    else:
+        direction = KickDirection.Up.value
+
+    return target_pos, direction
+
